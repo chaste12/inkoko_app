e0eb795ee03ec941d0df2282b683770d90ef468e
Gikundiro Chaste <gikundirochacha2002@gmail.com>
2021-12-16 15:50:33 +0200

    changes

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,3 +1,7 @@
+import 'dart:convert' as convert;
+
+import 'package:http/http.dart' as http;
+
 class Cat {
   String image;
   String name;
@@ -26,42 +30,63 @@ final cat = [
 ];
 
 class Products {
-  String image;
-  String productName;
-  String amount;
-  String companyName;
+  String title;
+  String availableQuantity;
+  String price;
+  String minimumQuantity;
+  String description;
 
   Products(
-      {required this.image,
-      required this.productName,
-      required this.amount,
-      required this.companyName});
+      {required this.title,
+      required this.availableQuantity,
+      required this.price,
+      required this.minimumQuantity,
+      required this.description});
+
+  // factory Products.fromJson(Map<String, dynamic> map) {
+  //   return Products(
+  //       title: map['title'],
+  //       availableQuantity: map['available_quantity'],
+  //       price: map['price'],
+  //       minimumQuantity: map['minimum_quantity'],
+  //       description: map['comment']);
+  // }
 }
 
 final product = [
   Products(
-      image: "assets/images/3.jpg",
-      productName: "Eggs",
-      amount: "1400",
-      companyName: "Gasogi Eggs"),
+      title: "assets/images/3.jpg",
+      availableQuantity: "Eggs",
+      price: "1400",
+      minimumQuantity: "Fablab Eggs",
+      description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis assumenda aliquid sit natus nam quaerat incidunt quas neque aut accusantium optio perferendis, possimus praesentium molestiae officia saepe temporibus enim!.'),
   Products(
-      image: "assets/images/istockphoto-157375734-170667a.jpg",
-      productName: "Chicks",
-      amount: "1400",
-      companyName: "Ella farmers"),
+      title: "assets/images/istockphoto-157375734-170667a.jpg",
+      availableQuantity: "Chicks",
+      price: "3400",
+      minimumQuantity: "Ella farmers",
+      description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis assumenda aliquid sit natus nam quaerat incidunt quas neque aut accusantium optio perferendis, possimus praesentium molestiae officia saepe temporibus enim!.'),
   Products(
-      image: "assets/images/katherine-chase-BzF1XBy5xOc-unsplash.jpg",
-      productName: "Eggs",
-      amount: "1400",
-      companyName: "Good Eggs"),
+      title: "assets/images/katherine-chase-BzF1XBy5xOc-unsplash.jpg",
+      availableQuantity: "Eggs",
+      price: "5400",
+      minimumQuantity: "Good Eggs",
+      description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis assumenda aliquid sit natus nam quaerat incidunt quas neque aut accusantium optio perferendis, possimus praesentium molestiae officia saepe temporibus enim!.'),
   Products(
-      image: "assets/images/obie-fernandez-KMDXqrESUuw-unsplash.jpg",
-      productName: "Chickens",
-      amount: "1400",
-      companyName: "Aquila Farmers"),
+      title: "assets/images/obie-fernandez-KMDXqrESUuw-unsplash.jpg",
+      availableQuantity: "Chickens",
+      price: "1400",
+      minimumQuantity: "Aquila Farmers",
+      description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis assumenda aliquid sit natus nam quaerat incidunt quas neque aut accusantium optio perferendis, possimus praesentium molestiae officia saepe temporibus enim!.'),
   Products(
-      image: "assets/images/jk-sloan-9zLa37VNL38-unsplash.jpg",
-      productName: "Chicken meat",
-      amount: "1400",
-      companyName: "Sera butchery"),
+      title: "assets/images/jk-sloan-9zLa37VNL38-unsplash.jpg",
+      availableQuantity: "Chicken meat",
+      price: "2800",
+      minimumQuantity: "Sera butchery",
+      description:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam debitis assumenda aliquid sit natus nam quaerat incidunt quas neque aut accusantium optio perferendis, possimus praesentium molestiae officia saepe temporibus enim!.'),
 ];
